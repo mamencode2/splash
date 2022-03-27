b4e0a31ebff2180afa4db346f4957cf82ba5e31e
mamencode2 <99823960+mamencode2@users.noreply.github.com>
2022-03-27 21:27:28 +0300

    Add files via upload

---
+++ src/views/splash-layout-edited339.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import styles from './splash-layout-edited339.module.css'
+
+const SplashLayoutEdited339 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className={styles['SplashLayoutEdited339']}>
+        <img
+          alt="splashpictureOne13310"
+          src="/playground_assets/splashpictureone13310-ojrh-500w.png"
+          className={styles['image']}
+        />
+        <img
+          alt="mainBg3311"
+          src="/playground_assets/mainbg3311-jd3-500w.png"
+          className={styles['image1']}
+        />
+        <img
+          alt="yellowRec3312"
+          src="/playground_assets/yellowrec3312-jgo-200h.png"
+          className={styles['image2']}
+        />
+        <img
+          alt="bottom3313"
+          src="/playground_assets/bottom3313-sm-500w.png"
+          className={styles['image3']}
+        />
+        <button className={styles['Button3315']}>
+          <button className={styles['ButtonYellowI3315336']}>
+            <img
+              alt="yelloButtonbackgroundI3315336342"
+              src="/playground_assets/yellobuttonbackgroundi3315336342-7z1v-200h.png"
+              className={styles['image4']}
+            />
+          </button>
+          <span className={styles['text']}>
+            <span className={styles['text1']}>Get Started</span>
+          </span>
+        </button>
+        <div className={styles['Frame13316']}>
+          <span className={styles['text2']}>
+            <span className={styles['text3']}>My</span>
+            <span className={styles['text4']}> Shop</span>
+          </span>
+          <span className={styles['text5']}>
+            <span className={styles['text6']}>
+              lorem Ipsm is simply dummy text of printing and typesetting
+              industry
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SplashLayoutEdited339
